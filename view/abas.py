2226a268c56097430797e563020c30250d7a0df1
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-05-27 19:34:41 -0300

    Update abas.py

--- view/abas.py
+++ view/abas.py
@@ -1,18 +1,14 @@
 import streamlit as st
 
 def exibir_abas():
-    aba1, aba2 = st.tabs(["Visão Financeira", "Evolução Mensal"])
-    with aba1:
-        tipo_visualizacao = st.radio(
-            "Qual forma de visualização?",
-            ["📊 Valores absolutos", "📉 Proporção percentual"],
-            horizontal=True,
-            label_visibility="collapsed")
+    tipo_visualizacao = st.radio(
+        "Qual forma de visualização?",
+        ["📊 Valores absolutos", "📉 Proporção percentual"],
+        horizontal=True,
+        label_visibility="collapsed")
 
-        if tipo_visualizacao == "📉 Proporção percentual":
-            agrupar_outros = True
-        else:
-            agrupar_outros = False
-    with aba2:
-        st.write("Nada Ainda")
+    if tipo_visualizacao == "📉 Proporção percentual":
+        agrupar_outros = True
+    else:
+        agrupar_outros = False
     return tipo_visualizacao,agrupar_outros
