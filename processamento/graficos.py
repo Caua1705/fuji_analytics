3098e6c4f2bcdf26cadd34d90c15ee7d7f247e86
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-05-26 21:03:10 -0300

    Update graficos.py

--- processamento/graficos.py
+++ processamento/graficos.py
@@ -29,7 +29,12 @@ def criar_graficos_principais_receitas(receitas_por_categoria, tipo_visualizacao
         st.plotly_chart(fig2, use_container_width=True)
 
 def criar_graficos_principais_despesas(despesas_por_categoria,tipo_visualizacao):
-    fig1=px.bar(despesas_por_categoria,x="Centro_Custo",y="Valor_Pago/Recebido",title="Despesas por Categoria")
+    fig1=px.bar(despesas_por_categoria,
+                x="Centro_Custo",
+                y="Valor_Pago/Recebido",
+                title="Despesas por Categoria",
+                text_auto=".2s",
+                color="Centro_Custo")
     fig2=px.pie(despesas_por_categoria,names="Centro_Custo",values="Valor_Pago/Recebido",title="Despesas por Categoria")
     if tipo_visualizacao=="📊 Valores absolutos":
         st.plotly_chart(fig1)
