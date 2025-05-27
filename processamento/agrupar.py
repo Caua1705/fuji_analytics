ac066110d6ed39ade7c66dfde0befa08d3cda719
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-05-26 22:36:15 -0300

    Update agrupar.py

--- processamento/agrupar.py
+++ processamento/agrupar.py
@@ -9,9 +9,9 @@ def agrupar_por_filial(df):
 def receitas_por_categoria(df_receitas):
     df_receitas["Grupo"]=df_receitas["Grupo"].replace({"COUVERT ARTISTICO":"COUVERT"})
     df_receitas_por_categoria=df_receitas.groupby("Grupo")["Valor"].sum().sort_values(ascending=False).reset_index()
-    if len(df_receitas_por_categoria) > 6:
-        top5 = df_receitas_por_categoria.iloc[:6]
-        outros_valor = df_receitas_por_categoria.iloc[6:]["Valor"].sum()
+    if len(df_receitas_por_categoria) > 9:
+        top5 = df_receitas_por_categoria.iloc[:9]
+        outros_valor = df_receitas_por_categoria.iloc[9:]["Valor"].sum()
         outros = pd.DataFrame([{"Grupo": "Outros", "Valor": outros_valor}])
         df_receitas_por_categoria = pd.concat([top5, outros], ignore_index=True)
     return df_receitas_por_categoria
@@ -24,9 +24,9 @@ def despesas_por_categoria(df_despesas):
         "LVP SERVIÇOS  ADMINISTRATIVOS LTDA":"LVP",
         "PUBLICIDADE E PROPAGANDA":"PUBLICIDADE"})
     df_despesas_por_categoria=df_despesas.groupby("Centro_Custo")["Valor_Pago/Recebido"].sum().sort_values(ascending=False).reset_index()
-    if len(df_despesas_por_categoria)>6:
-        top5=df_despesas_por_categoria.iloc[:6]
-        outros_valor = df_despesas_por_categoria.iloc[6:]["Valor_Pago/Recebido"].sum()
+    if len(df_despesas_por_categoria)>9:
+        top5=df_despesas_por_categoria.iloc[:9]
+        outros_valor = df_despesas_por_categoria.iloc[9:]["Valor_Pago/Recebido"].sum()
         outros = pd.DataFrame([{"Centro_Custo": "Outros", "Valor_Pago/Recebido": outros_valor}])
         df_despesas_por_categoria = pd.concat([top5, outros], ignore_index=True)
     return df_despesas_por_categoria
