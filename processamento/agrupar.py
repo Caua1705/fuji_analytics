228a196ff2704b183f1cbfadc234856a41951d54
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-05-26 21:41:38 -0300

    Update agrupar.py

--- processamento/agrupar.py
+++ processamento/agrupar.py
@@ -8,18 +8,18 @@ def agrupar_por_filial(df):
 
 def receitas_por_categoria(df_receitas):
     df_receitas_por_categoria=df_receitas.groupby("Grupo")["Valor"].sum().sort_values(ascending=False).reset_index()
-    if len(df_receitas_por_categoria) > 8:
-        top5 = df_receitas_por_categoria.iloc[:8]
-        outros_valor = df_receitas_por_categoria.iloc[8:]["Valor"].sum()
+    if len(df_receitas_por_categoria) > 7:
+        top5 = df_receitas_por_categoria.iloc[:7]
+        outros_valor = df_receitas_por_categoria.iloc[7:]["Valor"].sum()
         outros = pd.DataFrame([{"Grupo": "Outros", "Valor": outros_valor}])
         df_receitas_por_categoria = pd.concat([top5, outros], ignore_index=True)
     return df_receitas_por_categoria
 
 def despesas_por_categoria(df_despesas):
     df_despesas_por_categoria=df_despesas.groupby("Centro_Custo")["Valor_Pago/Recebido"].sum().sort_values(ascending=False).reset_index()
-    if len(df_despesas_por_categoria)>8:
-        top5=df_despesas_por_categoria.iloc[:8]
-        outros=df_despesas_por_categoria.iloc[8:].sum()
+    if len(df_despesas_por_categoria)>7:
+        top5=df_despesas_por_categoria.iloc[:7]
+        outros=df_despesas_por_categoria.iloc[7:].sum()
         top5.loc["Outros"]=outros
         return top5
     return df_despesas_por_categoria
