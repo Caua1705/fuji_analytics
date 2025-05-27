6fadb73fbe072cccf988a19f25218294a722c646
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-05-26 21:10:34 -0300

    Update graficos.py

--- processamento/graficos.py
+++ processamento/graficos.py
@@ -32,14 +32,14 @@ def criar_graficos_principais_receitas(receitas_por_categoria, tipo_visualizacao
 def criar_graficos_principais_despesas(despesas_por_categoria,tipo_visualizacao):
     fig1=px.bar(despesas_por_categoria,
                 x="Centro_Custo",
-                y="Valor_Pago/Recebido",
+                y="Valor",
                 title="Despesas por Categoria",
                 text_auto=".2s",
                 color="Centro_Custo")
     fig1.update_layout(xaxis_title="Centro_Custo",
                        yaxis_title="Valor_Pago/Recebido",
                        showlegend=False,
-                       xaxis_tickangle=0,
+                       xaxis_tickangle=-45,
                        )
     fig2=px.pie(despesas_por_categoria,names="Centro_Custo",values="Valor_Pago/Recebido",title="Despesas por Categoria")
     if tipo_visualizacao=="📊 Valores absolutos":
