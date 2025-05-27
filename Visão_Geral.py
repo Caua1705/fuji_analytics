201d650b7e0b66b8991cbda80784f6699738c9a8
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-05-27 17:17:13 -0300

    Update Visão_Geral.py

--- Visão_Geral.py
+++ Visão_Geral.py
@@ -14,36 +14,37 @@ from processamento.agrupar import agrupar_por_categoria
 from view.abas import exibir_abas
 #Exibir Gráficos
 from view.graficos import exibir_graficos
-
 st.set_page_config(layout="wide")
-logo_path = "assets/logo_fuji.png"
+st.markdown(
+    """
+    <style>
+    .block-container {
+        padding-top: 0.5rem;
+        padding-bottom: 10rem;
+    }
+    h1 {
+        margin: 0;
+        padding: 0;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 st.markdown(
     """
     <div style='
         display: flex; 
         align-items: center; 
-        gap: 8px; 
-        margin-top: -30px;
-        margin-left: -30px;
-        margin-bottom: -45px;
+        gap: 20px; 
+        margin-bottom: -30px;  /* <<< AQUI você controla a aproximação */
     '>
-        <img src='https://raw.githubusercontent.com/Caua1705/fuji_analytics/main/assets/novo_logo.png' 
-             width='150' 
-             style='margin: 0;'>
-        <h1 style='
-            margin: 0; 
-            font-size: 36px; 
-            margin-left: -10px;
-            color: #A67C52;
-            font-weight: 600;
-        '>
-            Painel Financeiro
-        </h1>
+        <img src="https://raw.githubusercontent.com/Caua1705/fuji_analytics/main/assets/novo_logo.png" width="180">
+        <h1 style="font-weight: 600; margin: 0;">Visão Estratégica</h1>
     </div>
     """,
     unsafe_allow_html=True
 )
-
 #Filtros Sidebar
 with st.sidebar:
     st.subheader("Filial")
