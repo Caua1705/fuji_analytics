129481a796f82e3da28b36860f14f001ddbe0fc0
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-05-28 14:17:55 -0300

    Update graficos.py

--- view/graficos.py
+++ view/graficos.py
@@ -6,7 +6,7 @@ import streamlit as st
 # 🔥 Gráfico de Barras
 def criar_graficos_barra(df_agrupado, tipo_df, x, y, filial):
     if tipo_df == "Receitas":
-        cores = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b"]
+        cores =['#D66BA0', '#8B5E3C', '#6BCBDB', '#F5CBA7', '#A9CCE3']
         titulo = f"Distribuição da Receita por Categoria - {filial}"
     else:
         cores = ["#17becf", "#bcbd22", "#e377c2", "#7f7f7f", "#ffbb78", "#aec7e8"]
