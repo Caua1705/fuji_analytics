0089f3ee13033984bcf17f9f06670c314c1cdc83
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-05-27 21:46:33 -0300

    Update abas.py

--- view/abas.py
+++ view/abas.py
@@ -1,37 +1,16 @@
 import streamlit as st
 
 def exibir_abas(filial):
-    # CSS para alinhar o toggle
-    st.markdown(
-    """
-    <style>
-    div[data-testid="stHorizontalBlock"] div[role="checkbox"] {
-        transform: translateY(-100px);
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-# Título centralizado e com menos espaçamento
-    st.markdown(
-    f"""
-    <h3 style='text-align: center; margin-bottom: 5px;'>Distribuição de Receita e Despesas | {filial}</h3>
-    """,
-    unsafe_allow_html=True
-)
-
-# Toggle alinhado à direita
-    col_esquerda, col_toggle = st.columns([9.5, 0.5])
-    with col_toggle:
+    col1, col2 = st.columns([5, 1])
+    with col1:
+        st.subheader(f"Distribuição de Receita e Despesas | {filial}")
+    with col2:
         modo_percentual = st.toggle("📊 %", help="Mostrar proporção percentual")
-
-    # Definindo tipo de visualização
     if modo_percentual:
         tipo_visualizacao = "Proporção percentual"
         agrupar_outros = True
     else:
         tipo_visualizacao = "Valores absolutos"
         agrupar_outros = False
+    return tipo_visualizacao,agrupar_outros
 
-    return tipo_visualizacao, agrupar_outros
