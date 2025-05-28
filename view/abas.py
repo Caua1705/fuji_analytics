76f2ddc636eeed73fdf75b21d2dc925838bf6ffc
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-05-27 22:19:14 -0300

    Update abas.py

--- view/abas.py
+++ view/abas.py
@@ -1,11 +1,12 @@
 import streamlit as st
 
 def exibir_abas():
-    col1, col2 = st.columns(2)
+    col1, col2,col3 = st.columns(3)
     with col1:
         st.subheader("Análise de Receitas")
     with col2:
         st.subheader("Análise de Despesas")
+    with col3:
         modo_percentual = st.toggle("Mostrar proporção (%)", value=False)
     if modo_percentual:
         tipo_visualizacao = "Proporção percentual"
