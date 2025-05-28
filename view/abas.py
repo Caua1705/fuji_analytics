191c07375f95425fe7107eaea126db30ba8c9911
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-05-27 21:38:53 -0300

    Update abas.py

--- view/abas.py
+++ view/abas.py
@@ -1,16 +1,20 @@
 import streamlit as st
 
 def exibir_abas(filial):
-    col1, col2 = st.columns([5, 1])
-    with col1:
-        st.subheader(f"Distribuição de Receita e Despesas | {filial}")
-    with col2:
+    st.markdown(
+        f"<h3 style='text-align: center;'>Distribuição de Receita e Despesas | {filial}</h3>",
+        unsafe_allow_html=True
+    )
+    _, col_toggle, _ = st.columns([5, 1, 5])
+    with col_toggle:
         modo_percentual = st.toggle("📊 %", help="Mostrar proporção percentual")
+
     if modo_percentual:
         tipo_visualizacao = "Proporção percentual"
         agrupar_outros = True
     else:
         tipo_visualizacao = "Valores absolutos"
         agrupar_outros = False
-    return tipo_visualizacao,agrupar_outros
+
+    return tipo_visualizacao, agrupar_outros
 
