f19a992e4cf8dfb55d05717a6772397eabef797e
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-06-02 17:40:06 -0300

    .

--- carregamento/carregar_dados.py
+++ carregamento/carregar_dados.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import streamlit as st
 from pathlib import Path
 
 dir_raiz=Path(__file__).parents[1]
 
+@st.cache_data
 def carregar_dataframes():
     df_receitas=pd.read_excel(dir_raiz / "data" / "receitas.xlsx")
     df_despesas = pd.read_excel(dir_raiz / "data" / "despesas.xlsx")
