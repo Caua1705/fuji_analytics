9226e4bf6ec5c1bd803acba7165437ac7315960b
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-06-02 09:40:23 -0300

    Update estilo.py

--- utils/estilo.py
+++ utils/estilo.py
@@ -12,14 +12,6 @@ def aplicar_estilo_pagina():
                 z-index: 100;
             }
 
-            /* 🔹 Reduzir espaço interno da página */
-            .block-container {
-                padding-top: 1.5rem;
-                padding-bottom: 1.5rem;
-                padding-left: 2rem;
-                padding-right: 2rem;
-            }
-
             .stMetric {
     background-color: #f9f9f9;
     border-radius: 10px;
